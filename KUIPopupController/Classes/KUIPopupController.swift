ce83542521c991e899e5eedc2e577d0d190a35a1
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-03-10 15:16:48 +0900

    라이브러리 및 샘플프로젝트 셋팅

---
+++ KUIPopupController/Classes/KUIPopupController.swift
@@ -0,0 +1,13 @@
+//
+//  KUIPopupController.swift
+//  KUIPopupController
+//
+//  Created by kofktu on 2017. 3. 10..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+import UIKit
+
+public final class KUIPopupController {
+    
+}
---
+++ KUIPopupController/KUIPopupController.h
@@ -0,0 +1,19 @@
+//
+//  KUIPopupController.h
+//  KUIPopupController
+//
+//  Created by kofktu on 2017. 3. 10..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIPopupController.
+FOUNDATION_EXPORT double KUIPopupControllerVersionNumber;
+
+//! Project version string for KUIPopupController.
+FOUNDATION_EXPORT const unsigned char KUIPopupControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIPopupController/PublicHeader.h>
+
+
